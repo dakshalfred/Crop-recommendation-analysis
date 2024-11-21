e66ffd334421b01ceda5c2e1393ac037d21cc710
dakshalfred <daksh.alfred@msea.christuniversity.in>
2024-11-21 08:37:32 +0530

    Update crop_recommendation.py

--- crop_recommendation.py
+++ crop_recommendation.py
@@ -125,7 +125,7 @@ if option == "Get Region Information":
     show_graph = st.checkbox("Show Graph")
     if show_graph:
         st.subheader("Graphical Representation")
-        fig, ax = plt.subplots(figsize=(9 * 0.6, 6 * 0.6))  # Resize chart to 60% of original size
+        fig, ax = plt.subplots(figsize=(9 * 0.3, 6 * 0.3))  # Resize chart to 60% of original size
         sns.barplot(data=filtered_data_region, x="Crop", y="Area", ax=ax)
         
         # Rotate x-axis labels to prevent overlap
